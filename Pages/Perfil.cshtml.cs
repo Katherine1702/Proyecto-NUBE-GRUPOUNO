93d4583c119a563ce25cb72b6e123a012989b381
Katherine1702 <valeskaes.rodriguez17@gmail.com>
2022-03-18 20:03:50 -0600

    Add project files.

---
+++ Pages/Perfil.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_NUBE_GRUPO1.Pages
+{
+    public class PerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
